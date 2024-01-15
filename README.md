b35409211c6cf526fea3faf476e8b7a6b14579a6
Simon Ziegler <simon.ziegler.1998+github@pm.me>
2024-01-15 19:46:56 +0100

    fixed bugs and added README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# Installation
+requires python version 3.12
+
+create a virtual environment and activate it and install the dependencies:
+
+	python -m venv .venv
+	./venv/Scripts/activate
+	python -m pip install -r requirements.txt
+
+# Usage
+create a directory named `origs` and put your audio files there, then run the script with
+
+	python main.py
--- main.py
+++ main.py
@@ -20,48 +20,57 @@ def main():
         if pth_ff.is_file():
             try:
                 # open the file as a soundfile
-                data, rate = soundfile.read(pth_ff)
+                data, f_rate = soundfile.read(pth_ff)
 
                 # store the soundfile in the dictionary
-                dct_audio_files[pth_ff] = {"audio": {"orig": data}, "rate": rate}
+                dct_audio_files[pth_ff] = {"audio": {"orig": data}, "rate": f_rate}
 
             # Error: file is no audio file
             except soundfile.LibsndfileError:
-                print(f"{pth_ff} is no audio-file")
+                print(f"{pth_ff} is no audio-file (copying original to output)")
 
                 # copy the file to the output directory
                 shutil.copy(pth_ff, pth_create_output_file_path(pth_ff))
 
 
-    # check for the audio-file with the lowest loudness
-    f_lowest_loudness = 0
+    # check for the audio-file with the lowest loudness crest
+    f_lowest_loudness_crest = 0
+
+    print ("analyzing files")
 
     for pth_ff, data in dct_audio_files.items():
         # peak normalize to -1 dbTP to compare the files
-        data["audio"]["-1 dbTP"] = pyloudnorm.normalize.peak(data["audio"]["orig"], -1)
+        data_1dbTP = pyloudnorm.normalize.peak(data["audio"]["orig"], -1)
 
         # create pyloudnorm-meters for each sample rate
         if data["rate"] not in dct_meter:
-            dct_meter["rate"] = pyloudnorm.Meter(rate)
+            dct_meter[data["rate"]] = pyloudnorm.Meter(f_rate)
 
-        # store the loudness of the peak-normalized file
-        data["loudness"] = {"-1 dbTP": dct_meter["rate"].integrated_loudness(data["audio"]["-1 dbTP"])}
+        # analyze the loudness of the original and the peak normalized file
+        data["loudness"] = {"orig": dct_meter[data["rate"]].integrated_loudness(data["audio"]["orig"])}
+
+        f_loudness_data_1dbTP = dct_meter[data["rate"]].integrated_loudness(data_1dbTP)
 
         # if the file has a new, lowest loudness, store this loudness
-        if data["loudness"]["-1 dbTP"] < f_lowest_loudness:
-            f_lowest_loudness = data["loudness"]["-1 dbTP"]
+        if f_loudness_data_1dbTP < f_lowest_loudness_crest:
+            f_lowest_loudness_crest = f_loudness_data_1dbTP
 
-            print (f"lowest loudness is {f_lowest_loudness:.2f} LUFS ({pth_ff})")
+            print (f"lowest loudness crest is {f_lowest_loudness_crest:.2f} LUFS ({pth_ff})")
+
+    print ("normalizing files")
 
     for pth_ff, data in dct_audio_files.items():
-        # normalize to the lowest found loudness to make all audio-files the same loudness
-        data["audio"]["normalised"] = pyloudnorm.normalize.loudness(data["audio"]["orig"], data["loudness"]["-1 dbTP"], f_lowest_loudness)
+        # normalize to the lowest found loudness crest to make all audio-files the same loudness
+        data["audio"]["normalized"] = pyloudnorm.normalize.loudness(data["audio"]["orig"], data["loudness"]["orig"], f_lowest_loudness_crest)
+        data["loudness"]["normalized"] = dct_meter[data["rate"]].integrated_loudness(data["audio"]["normalized"])
 
         pth_out_file = pth_create_output_file_path(pth_ff)
 
         # export the normalized audio
         if not pth_out_file.exists():
-            soundfile.write(pth_out_file, data["audio"]["normalised"], data["rate"])
+            soundfile.write(pth_out_file, data["audio"]["normalized"], data["rate"])
+
+            print (f"{pth_out_file}: {data["loudness"]["normalized"] - data["loudness"]["orig"]:.2f} dB")
         else:
             print (f"{pth_out_file} already exists - skipping")
 
